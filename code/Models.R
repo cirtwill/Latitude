ba345b177711b0069b544f44696d21a056f51e8c
cirtwill <alyssa.cirtwill@gmail.com>
2014-03-19 17:30:10 +1300

    Ran all models.

--- code/Models.R
+++ code/Models.R
@@ -1,7 +1,4 @@
-
-
-
-data=read.table('summary-properties.tsv',sep='\t',header=TRUE)
+data=read.table('../mod_data/summary-properties.tsv',sep='\t',header=TRUE)
 attach(data)
 
 ##################################
@@ -9,23 +6,17 @@ attach(data)
 ##		Part 1: testing S, C, LS over lat
 ##################################
 ##################################
-species_latbytype = glm(log10(Species)~Latitude*Ecotype+Year_pub)
-links_latbytype = glm(log10(LS)~Latitude*Ecotype+Year_pub)
-conn_latbytype = glm(log10(Connectance)~Latitude*Ecotype+Year_pub)
 
-#stepped models:
-species_stepped = glm(log10(Species)~Latitude+Ecotype+Year_pub)
-conn_stepped = glm(log10(Connectance)~Ecotype+Year_pub)
-links_stepped = glm(log10(LS)~Latitude+Ecotype+Year_pub)
+#Including year published as a randtom effect
 
-species_justlat = glm(log10(Species)~Latitude+Year_pub)
-links_justlat = glm(log10(LS)~Latitude+Year_pub)
-conn_justlat = glm(log10(Connectance)~Latitude+Year_pub)
+species_latbytype = glm(log10(Species)~Latitude*(Ecotype-1)+(1|Year_pub))
+links_latbytype = glm(log10(LS)~Latitude*(Ecotype-1)+(1|Year_pub))
+conn_latbytype = glm(log10(Connectance)~Latitude*(Ecotype-1)+(1|Year_pub))
 
-
-*Year published main significant thing, ask Daniel how to standardize for that. Inc. as random effect?
-* In additive ones, freshwater webs a little bit weird
-* Just latitude, species and links significant. Opposite to expected trend, but hey.
+#dredged models:
+species_min = glm(log10(Species)~Ecotype-1+(1|Year_pub))
+links_min = glm(log10(LS)~Ecotype-1+(1|Year_pub))
+conn_min = glm(log10(Connectance)~Ecotype-1+(1|Year_pub))
 
 ##################################
 ##################################
@@ -33,66 +24,70 @@ conn_justlat = glm(log10(Connectance)~Latitude+Year_pub)
 ##################################
 ##################################
 
-# Latitude
+linkSD_scale=glm(log10(LinkSD)~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+gen_scale=glm(log10(Gen)~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+Gendata=data[data$GenSD!=0,]
+genSD_scale=glm(log10(GenSD)~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+vul_scale=glm(log10(Vul)~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+vulSD_scale=glm(log10(VulSD)~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+mean_SWTL_scale=glm(log10(mean_SWTL)~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+max_SWTL_scale=glm(log10(max_SWTL)~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+path_scale=glm(log10(Path)~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+basal_scale=glm(pBas~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub),family='binomial')
+int_scale=glm(pInt~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub),family='binomial')
+top_scale=glm(pTop~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub),family='binomial')
+herb_scale=glm(pHerb~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub),family='binomial')
+omni_scale=glm(pOmni~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub),family='binomial')
 
-linkSD_scale=glm(log10(LinkSD)~Latitude*(log10(Connectance)+log10(Species)))
-gen_scale=glm(log10(Gen)~Latitude*(log10(Connectance)+log10(Species)))
-#genSD_scale=glm(log10(GenSD)~Latitude*(log10(Connectance)+log10(Species)))
-#log scale not working
-genSD_scale=glm(GenSD~Latitude*(log10(Connectance)+log10(Species)))
-vul_scale=glm(log10(Vul)~Latitude*(log10(Connectance)+log10(Species)))
-vulSD_scale=glm(log10(VulSD)~Latitude*(log10(Connectance)+log10(Species)))
-mean_SWTL_scale=glm(log10(mean_SWTL)~Latitude*(log10(Connectance)+log10(Species)))
-max_SWTL_scale=glm(log10(max_SWTL)~Latitude*(log10(Connectance)+log10(Species)))
-path_scale=glm(log10(Path)~Latitude*(log10(Connectance)+log10(Species)))
-#clus_scale=glm(log10(Clus)~Latitude*(log10(Connectance)+log10(Species)))
-#log-scaling isn't working here. Raw works.
-clus_scale=glm(Clus~Latitude*(log10(Connectance)+log10(Species)))
-basal_scale=glm(pBas~Latitude*(log10(Connectance)+log10(Species)),family='binomial')
-int_scale=glm(pInt~Latitude*(log10(Connectance)+log10(Species)),family='binomial')
-top_scale=glm(pTop~Latitude*(log10(Connectance)+log10(Species)),family='binomial')
-herb_scale=glm(pHerb~Latitude*(log10(Connectance)+log10(Species)),family='binomial')
-omni_scale=glm(pOmni~Latitude*(log10(Connectance)+log10(Species)),family='binomial')
+#Motifs
+x102_scale=glm(X102~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x108_scale=glm(X108~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x110_scale=glm(X110~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x12_scale=glm(X12~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x14_scale=glm(X14~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x238_scale=glm(X238~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x36_scale=glm(X36~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x38_scale=glm(X38~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x46_scale=glm(X46~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x6_scale=glm(X6~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x74_scale=glm(X74~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x78_scale=glm(X78~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x98_scale=glm(X98~(Latitude+Ecotype)*(log10(Connectance)+log10(Species))+(1|Year_pub))
 
-# both
+#Dredging in now.
 
-linkSD_both=glm(log10(LinkSD)~Latitude*Ecotype*(log10(Connectance)+log10(Species)))
-gen_both=glm(log10(Gen)~Latitude*Ecotype*(log10(Connectance)+log10(Species)))
-#genSD_both=glm(log10(GenSD)~Latitude*Ecotype*(log10(Connectance)+log10(Species)))
-#log scale not working
-genSD_both=glm(GenSD~Latitude*Ecotype*(log10(Connectance)+log10(Species)))
-vul_both=glm(log10(Vul)~Latitude*Ecotype*(log10(Connectance)+log10(Species)))
-vulSD_both=glm(log10(VulSD)~Latitude*Ecotype*(log10(Connectance)+log10(Species)))
-mean_SWTL_both=glm(log10(mean_SWTL)~Latitude*Ecotype*(log10(Connectance)+log10(Species)))
-max_SWTL_both=glm(log10(max_SWTL)~Latitude*Ecotype*(log10(Connectance)+log10(Species)))
-path_both=glm(log10(Path)~Latitude*Ecotype*(log10(Connectance)+log10(Species)))
-#clus_both=glm(log10(Clus)~Latitude*Ecotype*(log10(Connectance)+log10(Species)))
-#log-scaling isn't working here. Raw works.
-clus_both=glm(Clus~Latitude*Ecotype*(log10(Connectance)+log10(Species)))
-basal_both=glm(pBas~Latitude*Ecotype*(log10(Connectance)+log10(Species)),family='binomial')
-int_both=glm(pInt~Latitude*Ecotype*(log10(Connectance)+log10(Species)),family='binomial')
-top_both=glm(pTop~Latitude*Ecotype*(log10(Connectance)+log10(Species)),family='binomial')
-herb_both=glm(pHerb~Latitude*Ecotype*(log10(Connectance)+log10(Species)),family='binomial')
-omni_both=glm(pOmni~Latitude*Ecotype*(log10(Connectance)+log10(Species)),family='binomial')
+linkSD_dredged=glm(log10(LinkSD)~Ecotype*log10(Connectance)+log10(Species)+(1|Year_pub))
+gen_dredged=glm(log10(Gen)~(Latitude*log10(Species)+Ecotype+log10(Connectance))+(1|Year_pub))
+# 5 (very small) webs with 0 Generality is messing with this. Eliminating  1  4 30 84 85 for this model.
+genSD_dredged=with(Gendata,glm(GenSD~log10(Connectance)+log10(Species)+(1|Year_pub)))
+vul_dredged=glm(log10(Vul)~log10(Connectance)+log10(Species)+(1|Year_pub))
+vulSD_dredged=glm(log10(VulSD)~Latitude+Ecotype+log10(Connectance)+log10(Species)+(1|Year_pub))
+mean_SWTL_dredged=glm(log10(mean_SWTL)~(Latitude+Ecotype)*log10(Species)+(1|Year_pub))
+max_SWTL_dredged=glm(log10(max_SWTL)~Latitude*log10(Species)+Ecotype*log10(Connectance)+(1|Year_pub))
+path_dredged=glm(log10(Path)~Latitude+Ecotype*log10(Connectance)+(1|Year_pub))
+basal_dredged=glm(pBas~(1|Year_pub),family='binomial')
+int_dredged=glm(pInt~Latitude*(log10(Connectance)+log10(Species))+(1|Year_pub),family='binomial')
+top_dredged=glm(pTop~log10(Connectance)+(1|Year_pub),family='binomial')
+herb_dredged=glm(pHerb~(1|Year_pub),family='binomial')
+omni_dredged=glm(pOmni~(1|Year_pub),family='binomial')
 
-# Ecotype
+#Ecotype
+x102_dredged=glm(X102~Ecotype*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x108_dredged=glm(X108~Ecotype*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x110_dredged=glm(X110~Ecotype*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x14_dredged=glm(X14~Ecotype*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x238_dredged=glm(X238~Ecotype*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x46_dredged=glm(X46~Ecotype*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x74_dredged=glm(X74~Ecotype*(log10(Connectance)+log10(Species))+(1|Year_pub))
+x78_dredged=glm(X78~Ecotype*(log10(Connectance)+log10(Species))+(1|Year_pub))
 
-linkSD_eco=glm(log10(LinkSD)~Ecotype*(log10(Connectance)+log10(Species)))
-gen_eco=glm(log10(Gen)~Ecotype*(log10(Connectance)+log10(Species)))
-#genSD_eco=glm(log10(GenSD)~Ecotype*(log10(Connectance)+log10(Species)))
-#log scale not working
-genSD_eco=glm(GenSD~Ecotype*(log10(Connectance)+log10(Species)))
-vul_eco=glm(log10(Vul)~Ecotype*(log10(Connectance)+log10(Species)))
-vulSD_eco=glm(log10(VulSD)~Ecotype*(log10(Connectance)+log10(Species)))
-mean_SWTL_eco=glm(log10(mean_SWTL)~Ecotype*(log10(Connectance)+log10(Species)))
-max_SWTL_eco=glm(log10(max_SWTL)~Ecotype*(log10(Connectance)+log10(Species)))
-path_eco=glm(log10(Path)~Ecotype*(log10(Connectance)+log10(Species)))
-#clus_eco=glm(log10(Clus)~Ecotype*(log10(Connectance)+log10(Species)))
-#log-scaling isn't working here. Raw works.
-clus_eco=glm(Clus~Ecotype*(log10(Connectance)+log10(Species)))
-basal_eco=glm(pBas~Ecotype*(log10(Connectance)+log10(Species)),family='binomial')
-int_eco=glm(pInt~Ecotype*(log10(Connectance)+log10(Species)),family='binomial')
-top_eco=glm(pTop~Ecotype*(log10(Connectance)+log10(Species)),family='binomial')
-herb_eco=glm(pHerb~Ecotype*(log10(Connectance)+log10(Species)),family='binomial')
-omni_eco=glm(pOmni~Ecotype*(log10(Connectance)+log10(Species)),family='binomial')
+#No predictor
+x12_dredged=glm(X12~log10(Connectance)+log10(Species)+(1|Year_pub))
+x36_dredged=glm(X36~log10(Connectance)+log10(Species)+(1|Year_pub))
+x6_dredged=glm(X6~log10(Connectance)+log10(Species)+(1|Year_pub))
+
+#Latitude
+x108_dredged=glm(X108~Latitude*log10(Connectance)+(1|Year_pub))
+
+
 
