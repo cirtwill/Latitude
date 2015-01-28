a0e847d6d416e1da2922f28d571a5fa207552571
cirtwill <alyssa.cirtwill@gmail.com>
2015-01-28 17:22:00 +1300

    Have scaling plots... scaling doesn't look too important

--- code/property_plots.py
+++ code/property_plots.py
@@ -10,6 +10,10 @@ from PyGrace.colors import RandomColorScheme, MarkovChainColorScheme, ColorBrewe
 from PyGrace.dataset import SYMBOLS
 from PyGrace.Extensions.panel import Panel,MultiPanelGrace
 from PyGrace.drawing_objects import DrawText
+from PyGrace.axis import LINEAR_SCALE, LOGARITHMIC_SCALE
+
+from PyGrace.Extensions.colorbar import SolidRectangle, ColorBar
+from PyGrace.Styles.el import ElGraph, ElLinColorBar, ElLogColorBar
 
 # Think I want plots of the raw data (S, LS, G, V vs. lat) with slope lines
 # Plus predictions for LS, G, V vs S (with corrected obs.)
@@ -36,6 +40,54 @@ def datareader(rawdatafile):
       points['V'][ecotype].append((Latitude,V))
   f.close()
   return points
+
+
+def fixed_reader(coefffile):
+  fixed_effects={}
+  f=open(coefffile,'r')
+  for line in f:
+    if line.split()[0]!='"Estimate"':
+      name=line.split()[0][1:-1]
+      beta=float(line.split()[1])
+      fixed_effects[name]=beta
+  f.close()
+  return fixed_effects
+
+def correct_raw_obs(rawdatafile,coefffile):
+  # Would probably be more helpful to do a heatmap of observed latitudes.
+  corrected_obs={'Lake':{'tropical':[],'temperate':[],'arctic':[]},
+                'Stream':{'tropical':[],'temperate':[],'arctic':[]},
+                'Marine':{'tropical':[],'temperate':[],'arctic':[]},
+                'other':{'tropical':[],'temperate':[],'arctic':[]}}
+  fixed_effects=fixed_reader(coefffile)
+  prop=coefffile.split('/')[-1].split('_')[0]
+  f=open(rawdatafile,'r')
+  for line in f:
+    if line.split()[0]!='Web':
+      Latitude=float(line.split('\t')[6])
+      Species=int(line.split('\t')[7])
+      LS=float(line.split('\t')[10])
+      Gen=float(line.split('\t')[12])
+      Vul=float(line.split('\t')[14])
+      ecotype=line.split('\t')[1]
+      if ecotype=='lake':
+        ecotype='Lake'
+      elif ecotype=='marine':
+        ecotype='Marine'
+      elif ecotype=='stream':
+        ecotype='Stream'
+      else:
+        ecotype='other'
+
+      if Latitude<23.437:
+        corrected_obs[ecotype]['tropical'].append((Species,eval(prop)))
+      elif 23.437<=Latitude<66.563:
+        corrected_obs[ecotype]['temperate'].append((Species,eval(prop)))
+      elif Latitude>=66.563:
+        corrected_obs[ecotype]['arctic'].append((Species,eval(prop)))
+
+  f.close()
+  return corrected_obs
 
 def predictionreader(predfile):
   # Add slopelines (separate slopes for each type?)
@@ -140,17 +192,36 @@ def latplots(rawdatafile):
   grace.write_file(outfile1)
 
 def scaleplots(rawdatafile,predfolder):
+
+  xwidth=2
+  ywidth=.5
+
   outfile1='../manuscript/Figures/scaling_with_S.eps'
   outfile2='../manuscript/Figures/scaling_with_S.jpg'
 
   rawdata=datareader(rawdatafile)
 
-  grace=MultiPanelGrace(colors=ColorBrewerScheme('Paired'))
+  grace=MultiPanelGrace(colors=ColorBrewerScheme('Blues'))
+
   for prop in ['LS','Gen','Vul']:
+    coefffile='../mod_data/coefficients/'+prop+'_co.tsv'
     preddata=predictionreader(predfolder+prop+'.tsv')
-    print preddata.keys()
+    fixed=fixed_reader(coefffile)
+
     for ecotype in ['other','Marine','Lake','Stream']:
       graph=grace.add_graph(Panel)
+
+      temppoints=graph.add_dataset(correct_raw_obs(rawdatafile,coefffile)[ecotype]['temperate'])
+      troppoints=graph.add_dataset(correct_raw_obs(rawdatafile,coefffile)[ecotype]['tropical'])
+      arctpoints=graph.add_dataset(correct_raw_obs(rawdatafile,coefffile)[ecotype]['arctic'])
+
+      troppoints.line.linestyle=0
+      temppoints.line.linestyle=0
+      arctpoints.line.linestyle=0
+
+      troppoints.symbol.configure(size=.5,color=5,fill_color=5)
+      temppoints.symbol.configure(size=.5,color=8,fill_color=8)
+      arctpoints.symbol.configure(size=.5,color=10,fill_color=10)
 
       preddataset1=graph.add_dataset(preddata[ecotype][0])
       preddataset2=graph.add_dataset(preddata[ecotype][45])
@@ -160,49 +231,47 @@ def scaleplots(rawdatafile,predfolder):
       preddataset2.symbol.shape=0
       preddataset3.symbol.shape=0
 
-      preddataset1.line.linestyle=1
-      preddataset2.line.linestyle=1
-      preddataset3.line.linestyle=1
+      preddataset1.line.configure(linestyle=1,color=5)
+      preddataset2.line.configure(linestyle=1,color=8)
+      preddataset3.line.configure(linestyle=1,color=10)
+
       if ecotype=='other':
-        preddataset1.line.color=3
-        preddataset2.line.color=3
-        preddataset3.line.color=3
         if prop=='LS':
           graph.xaxis.label.configure(text='Estuary & Terrestrial',place='opposite',char_size=.75)
       elif ecotype=='Marine':
-        preddataset1.line.color=7
-        preddataset2.line.color=7
-        preddataset3.line.color=7
         if prop=='LS':
           graph.xaxis.label.configure(text='Marine',place='opposite',char_size=.75)
       elif ecotype=='Lake':
-        preddataset1.line.color=9
-        preddataset2.line.color=9
-        preddataset3.line.color=9
         if prop=='LS':
           graph.xaxis.label.configure(text='Lake',place='opposite',char_size=.75)
       else:
-        preddataset1.line.color=11
-        preddataset2.line.color=11
-        preddataset3.line.color=11
         if prop=='LS':
           graph.xaxis.label.configure(text='Stream',place='opposite',char_size=.75)
+          preddataset1.legend='0 degrees'
+          preddataset2.legend='45 degrees'
+          preddataset3.legend='75 degrees'
 
-
+          troppoints.legend='Tropical networks'
+          temppoints.legend='Temperate networks'
+          arctpoints.legend='Arctic networks'
+          graph.legend.configure(loc=(210,30),loctype='world',char_size=.75)
+          graph.legend.box_linestyle=0
       graph.world.xmin=0
       graph.world.xmax=200
       graph.xaxis.tick.configure(major=50,minor_ticks=1,major_size=.7,minor_size=.4,major_linewidth=1,minor_linewidth=1)
       graph.xaxis.ticklabel.configure(char_size=.5)
-      # graph.xaxis.label.configure(text="Absolute latitude (degrees)",char_size=.75)
+      graph.frame.linewidth=1
+      graph.xaxis.bar.linewidth=1
+      graph.yaxis.bar.linewidth=1
 
       graph.world.ymin=0
       graph.world.ymax=30
       graph.yaxis.tick.major=10
-      if prop=='LS' and ecotype=='estuary':
+      if prop=='LS' and ecotype=='other':
         graph.yaxis.label.text="Links/species"
-      elif prop=='Gen' and ecotype=='estuary':
+      elif prop=='Gen' and ecotype=='other':
         graph.yaxis.label.text="Generality"
-      elif prop=='Vul' and ecotype=='estuary':
+      elif prop=='Vul' and ecotype=='other':
         graph.yaxis.label.text="Vulnerability"
 
       graph.yaxis.tick.configure(minor_ticks=1,major_size=.7,minor_size=.4,major_linewidth=1,minor_linewidth=1)
