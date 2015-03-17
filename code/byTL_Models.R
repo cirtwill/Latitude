baef2c573380845494d5e0a02c312c2efc906480
cirtwill <alyssa.cirtwill@gmail.com>
2015-03-17 16:21:43 +1300

    Solved the non-log problem by deleting bad matrices

--- code/byTL_Models.R
+++ code/byTL_Models.R
@@ -1,4 +1,3 @@
-
 ## Power analysis function from http://www.esapubs.org/archive/ecol/E092/160/Sup_2_Guidelines.r
 ## Loading required packages. May need to be installed from CRAN.
 library(nlrwr)
@@ -8,9 +7,19 @@ library(MuMIn)
 
 source('Models.R')  # Load the other models, power_analysis
 
+
+# Set up for proportions and numbers, proportions first.
+
+
+
 ##############################################################################################
 ##############################################################################################
 for(infile in c('../mod_data/summary-properties.tsv','../non_TS/summary-properties.tsv')){
+
+  data=subset(data,data$Basal>0)
+  data=subset(data,data$Intermediate>0)
+  data=subset(data,data$Toppreds>0)
+  # Eliminate zeros. Don't care so much about herbies
 
   q=data$Species
   x=data$LS
@@ -19,89 +28,128 @@ for(infile in c('../mod_data/summary-properties.tsv','../non_TS/summary-properti
 
   # Not working because of zeros...
   power_analysis(data$Species,data$Basal)   #To determine whether scaling of LS~S is more lognormal or nonlinear.
-                        #It is lognormal, AIC=766.8 vs 1150.3 with nonlinear.
-  power_analysis(data$Species,data$Herbivores)   #To determine whether scaling of Gen~S is more lognormal or nonlinear.
-                        #It is lognormal, AIC=1037.4 vs 1265.9 with nonlinear.
+                        #It is lognormal, AIC=-141 vs -112 with nonlinear.
+  # power_analysis(data$Species,data$Herbivores)   #To determine whether scaling of Gen~S is more lognormal or nonlinear.
+  # Too many zeros.
   power_analysis(data$Species,data$Intermediate)   #To determine whether scaling of Vul~S is more lognormal or nonlinear.
-                        #It is lognormal, AIC=766.9 vs 1150.6
+                        #It is lognormal, AIC=-121.4 vs -72.6
   power_analysis(data$Species,data$Toppreds)   #To determine whether scaling of Vul~S is more lognormal or nonlinear.
+                        #It is lognormal, AIC=-163.5 vs -138.7
 
-  #Therefore using lognormal with confidence for modified models.
-  LS_full=with(data,lm(log10(LS)~log10(Species)
-    +log10(Species):Latitude
-    +log10(Species):(Stream+Lake+Marine+Terr)
-    +log10(Species):Latitude:(Stream+Lake+Marine+Terr)
-    ,na.action=na.fail))
-  ls_dredge=dredge(LS_full,rank=AIC)
-  if(infile=='../mod_data/summary-properties.tsv'){
-    LS_min =with(data,lm(log10(LS)~log10(Species)
-    +log10(Species):(Lake+Marine+Stream)
-    +log10(Species):Latitude
-    +log10(Species):Latitude:Lake
-    +log10(Species):Latitude:Stream
-    ,na.action=na.fail))    } else {
-    LS_min=with(data,lm(log10(LS)~log10(Species)
-    +log10(Species):(Lake+Marine+Terr)
-    +log10(Species):Latitude
-    +log10(Species):Latitude:Marine
-    +log10(Species):Latitude:Terr,
-    na.action=na.fail))    }
+  source('recreate_with_subset.R')
 
-  Gen_full=with(data,lm(log10(Gen)~log10(Species)
-    +log10(Species):Latitude
-    +log10(Species):(Stream+Lake+Marine+Terr)
-    +log10(Species):Latitude:(Stream+Lake+Marine+Terr)
+
+    LS_B_full=with(data,lm(log10(LS)~log10(Basal)
+    +log10(Basal):Latitude
+    +log10(Basal):(Stream+Lake+Marine+Terr)
+    +log10(Basal):Latitude:(Stream+Lake+Marine+Terr)
     ,na.action=na.fail))
-  g_dredge=dredge(Gen_full,rank=AIC)
-  if(infile=='../mod_data/summary-properties.tsv'){
-    Gen_min=with(data,lm(log10(Gen)~log10(Species)
-      +log10(Species):(Lake+Marine+Stream)
-      +log10(Species):Latitude
-      +log10(Species):Latitude:Lake
-      +log10(Species):Latitude:Stream
-      ,na.action=na.fail)) } else {
-    Gen_min=with(data,lm(log10(Gen)~log10(Species)
-      +log10(Species):(Lake+Marine+Terr)
-      +log10(Species):Latitude
-      +log10(Species):Latitude:Lake
-      ,na.action=na.fail))
+    lbdredge=dredge(LS_B_full,rank=AIC)
+    if(infile=='../non_TS/summary-properties.tsv'){
+      LS_B_min=with(data,lm(log10(LS)~log10(Basal)
+        +log10(Basal):Latitude
+        +log10(Basal):(Marine+Stream+Terr)
+        +log10(Basal):Latitude:(Marine+Terr) , na.action=na.fail   ))
     }
 
-  Vul_full=with(data,lm(log10(Vul)~log10(Species)
-    +log10(Species):Latitude
-    +log10(Species):(Stream+Lake+Marine+Terr)
-    +log10(Species):Latitude:(Stream+Lake+Marine+Terr)
+    # And if I use number
+    LS_B_full=with(data,lm(log10(LS)~log10(Basal*Species)
+    +log10(Basal*Species):Latitude
+    +log10(Basal*Species):(Stream+Lake+Marine+Terr)
+    +log10(Basal*Species):Latitude:(Stream+Lake+Marine+Terr)
     ,na.action=na.fail))
-  v_dredge=dredge(Vul_full,rank=AIC)
-  if(infile=='../mod_data/summary-properties.tsv'){
-    Vul_min=with(data,lm(log10(Vul)~log10(Species)
-      +log10(Species):(Lake+Marine+Stream)
-      +log10(Species):Latitude
-      +log10(Species):Latitude:Lake
-      7+log10(Species):Latitude:Stream
-      ,na.action=na.fail)) } else {
-    Vul_min=with(data,lm(log10(Vul)~log10(Species)
-      +log10(Species):(Lake+Marine+Terr)
-      +log10(Species):Latitude
-      +log10(Species):Latitude:Marine
-      +log10(Species):Latitude:Terr
-      ,na.action=na.fail))  }
+    lbdredge=dredge(LS_B_full,rank=AIC)
+    if(infile=='../non_TS/summary-properties.tsv'){
+      LS_B_min=with(data,lm(log10(LS)~log10(Basal*Species)+log10(Basal*Species):Marine)) }
+    # Boooooooring
+
+    LS_I_full=with(data,lm(log10(LS)~log10(Intermediate)
+    +log10(Intermediate):Latitude
+    +log10(Intermediate):(Stream+Lake+Marine+Terr)
+    +log10(Intermediate):Latitude:(Stream+Lake+Marine+Terr)
+    ,na.action=na.fail))
+    lidredge=dredge(LS_I_full,rank=AIC)
+    if(infile=='../non_TS/summary-properties.tsv'){
+      LS_B_min=with(data,lm(log10(LS)~log10(Intermediate)
+        +log10(Intermediate):(Lake+Stream), na.action=na.fail   ))
+    }
+
+    # And with number
+    LS_I_full=with(data,lm(log10(LS)~log10(Intermediate*Species)
+    +log10(Intermediate*Species):Latitude
+    +log10(Intermediate*Species):(Stream+Lake+Marine+Terr)
+    +log10(Intermediate*Species):Latitude:(Stream+Lake+Marine+Terr)
+    ,na.action=na.fail))
+    lidredge=dredge(LS_I_full,rank=AIC)
+    if(infile=='../non_TS/summary-properties.tsv'){
+      LS_I_min=with(data,lm(log10(LS)~log10(Intermediate*Species)
+        +log10(Intermediate*Species):(Lake+Marine+Stream+Terr)
+        +log10(Intermediate*Species):Latitude
+        +log10(Intermediate*Species):Latitude:(Lake+Stream) )) 
+    } # Dang, I was hoping this'd be simple....
+
+    LS_T_full=with(data,lm(log10(LS)~log10(Toppreds)
+    +log10(Toppreds):Latitude
+    +log10(Toppreds):(Stream+Lake+Marine+Terr)
+    +log10(Toppreds):Latitude:(Stream+Lake+Marine+Terr)
+    ,na.action=na.fail))
+    ltdredge=dredge(LS_T_full,rank=AIC)
+    if(infile=='../non_TS/summary-properties.tsv'){
+      LS_T_min=with(data,lm(log10(LS)~log10(Toppreds)
+      +log10(Toppreds):(Lake+Marine+Stream+Terr)
+      +log10(Toppreds):Latitude
+      +log10(Toppreds):Latitude:(Stream+Terr) ))
+    }
+
+    # And with number
+    LS_T_full=with(data,lm(log10(LS)~log10(Toppreds*Species)
+    +log10(Toppreds*Species):Latitude
+    +log10(Toppreds*Species):(Stream+Lake+Marine+Terr)
+    +log10(Toppreds*Species):Latitude:(Stream+Lake+Marine+Terr)
+    ,na.action=na.fail))
+    ltdredge=dredge(LS_T_full,rank=AIC)
+
+    LS_T_min=with(data,lm(log10(LS)~log10(Toppreds*Species)
+    +log10(Toppreds*Species):Latitude
+    +log10(Toppreds*Species):(Lake+Marine+Terr)
+    +log10(Toppreds*Species):Latitude:(Lake)
+    ,na.action=na.fail))
+    # And both of these are interesting. 
+
 
   # What's the basic correlation with latitude?
-  Sp_latdirect_full=(with(data,lm(Species~Latitude*(Stream+Lake+Marine+Terr),na.action=na.fail)))
-  LS_latdirect_full=(with(data,lm(LS~Latitude*(Stream+Lake+Marine+Terr),na.action=na.fail)))
-  Gen_latdirect_full=(with(data,lm(Gen~Latitude*(Stream+Lake+Marine+Terr),na.action=na.fail)))
-  Vul_latdirect_full=(with(data,lm(Vul~Latitude*(Stream+Lake+Marine+Terr),na.action=na.fail)))
+  B_latdirect_full=(with(data,lm(Basal~Latitude*(Stream+Lake+Marine+Terr),na.action=na.fail)))
+  I_latdirect_full=(with(data,lm(Intermediate~Latitude*(Stream+Lake+Marine+Terr),na.action=na.fail)))
+  T_latdirect_full=(with(data,lm(Toppreds~Latitude*(Stream+Lake+Marine+Terr),na.action=na.fail)))
+  H_latdirect_full=(with(data,lm(Herbivores~Latitude*(Stream+Lake+Marine+Terr),na.action=na.fail)))
 
-  sld=dredge(Sp_latdirect_full,rank=AIC)
-  lld=dredge(LS_latdirect_full,rank=AIC)
-  gld=dredge(Gen_latdirect_full,rank=AIC)
-  vld=dredge(Vul_latdirect_full,rank=AIC)
+  bld=dredge(B_latdirect_full,rank=AIC)
+  ild=dredge(I_latdirect_full,rank=AIC)
+  tld=dredge(T_latdirect_full,rank=AIC)
+  hld=dredge(H_latdirect_full,rank=AIC)
 
-  Sp_latdirect=(with(data,lm(Species~Stream),na.action=na.fail))
-  LS_latdirect=(with(data,lm(LS~1),na.action=na.fail))
-  Gen_latdirect=(with(data,lm(Gen~Terr+Lake),na.action=na.fail))
-  Vul_latdirect=(with(data,lm(Vul~1),na.action=na.fail))
+  B_latdirect=(with(data,lm(Basal~Latitude*Stream+Lake),na.action=na.fail))
+  I_latdirect=(with(data,lm(Intermediate~1),na.action=na.fail))
+  T_latdirect=(with(data,lm(Toppreds~Latitude*Lake+Stream+Marine+Latitude:Stream ,na.action=na.fail))
+  H_latdirect=(with(data,lm(Herbivores~Latitude+Lake),na.action=na.fail))
+
+
+  B_latdirect_full_num=(with(data,lm(Basal*Species~Latitude*(Stream+Lake+Marine+Terr),na.action=na.fail)))
+  I_latdirect_full_num=(with(data,lm(Intermediate*Species~Latitude*(Stream+Lake+Marine+Terr),na.action=na.fail)))
+  T_latdirect_full_num=(with(data,lm(Toppreds*Species~Latitude*(Stream+Lake+Marine+Terr),na.action=na.fail)))
+  H_latdirect_full_num=(with(data,lm(Herbivores*Species~Latitude*(Stream+Lake+Marine+Terr),na.action=na.fail)))
+
+  bldn=dredge(B_latdirect_full_num,rank=AIC)
+  ildn=dredge(I_latdirect_full_num,rank=AIC)
+  tldn=dredge(T_latdirect_full_num,rank=AIC)
+  hldn=dredge(H_latdirect_full_num,rank=AIC)
+
+  B_latdirect_num=(with(data,lm(Basal~Latitude*Stream),na.action=na.fail))
+  I_latdirect_num=(with(data,lm(Intermediate~Latitude*Stream+Lake+Marine+Terr),na.action=na.fail))
+  T_latdirect_num=(with(data,lm(Toppreds~Lake+Marine ,na.action=na.fail))
+  H_latdirect_num=(with(data,lm(Herbivores~Latitude*Stream+Lake+Marine),na.action=na.fail))
+
+  #Both number and proportion look roughly equally interesting....
 
   # Make some predictions for plotting
   # Observed species range is 3!-169.
@@ -177,8 +225,8 @@ for(infile in c('../mod_data/summary-properties.tsv','../non_TS/summary-properti
   colnames(Vul_fake)=c("Species","Latitude","Lake","Marine","Stream","pred")
 
   if(infile=='../mod_data/summary-properties.tsv'){
-    outdir='../mod_data/'  } else {
-      outdir='../non_TS/'     }
+    outdir='../mod_data/subset/'  } else {
+      outdir='../non_TS/subset/'     }
 
   save.image(file=paste(outdir,'Models.RData',sep=''))
   write.table(LS_fake,file=paste(outdir,'predictions/LS.tsv',sep=''),col.names=TRUE,row.names=FALSE,sep='\t')
