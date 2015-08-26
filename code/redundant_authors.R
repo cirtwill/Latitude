a89bc072d0e1bb4eb8ef41c4806680f5a7df164b
cirtwill <alyssa.cirtwill@gmail.com>
2015-08-26 15:28:43 +1200

    This is the list of which authors are redundant with whom. There are 169 unique authors.

---
+++ code/redundant_authors.R
@@ -0,0 +1,276 @@
+  # Here are the redundant authors I'm removing
+
+# Redundant with Egloff:
+  data$X38_coauthors <- NULL
+  data$Richardson <- NULL
+
+# Redundant with Abele
+  data$Simberloff <- NULL
+
+# Redundant with Ali_Dawah
+  data$Ali_Dawah <- NULL
+  data$Banasek.Richter <- NULL
+  data$Berlow <- NULL
+  data$Bersier <- NULL
+  data$Blanchard <- NULL
+  data$Brey <- NULL
+  data$Brose <- NULL
+  data$Cushing <- NULL
+  data$Dell <- NULL
+  data$Harper.Smith <- NULL
+  data$Jacob <- NULL
+  data$Knapp <- NULL
+  data$Ledger <- NULL
+  data$Memmot <- NULL
+  data$Mintenbeck <-NULL
+  data$Pinnegar <- NULL
+  data$Rall <- NULL
+  data$Ruess <- NULL
+  data$Ulrich <- NULL
+  data$Yodzis <- NULL
+
+# Redundant with Aloisio
+  data$Carvalho <- NULL
+
+# Redundant with Althaus
+  data$Bax <- NULL
+  data$Bulman <- NULL
+  data$He <- NULL
+  data$Williams_A <- NULL
+
+# Redundant with Amundsen
+  data$Klemetsen <- NULL
+  data$Knudsen <- NULL
+  data$Kristoffersen <- NULL
+  data$ Primicerio <- NULL
+
+# Redundant with Antezana
+  data$Cornejo.Donoso <- NULL
+
+# Redundant with Arreguin_Sanchez
+	data$Zetina_Rejon <- NULL
+
+# Redundant with Baeta
+	data$Niquil <- NULL
+
+# Redundant with Bailey.Watts
+	data$Bindloss <- NULL
+	data$Holden <- NULL
+	data$Smith_I <- NULL
+
+# Redundant with Beaney
+	data$Huxham <- NULL
+	data$Raffaelli <- NULL
+
+# Redundant with Bradstreet
+	data$Cross <- NULL
+
+# Redundant with Bretschko
+	data$Gollmann <- NULL
+	data$Pechlaner <- NULL
+	data$Pfeifer <- NULL
+	data$Tilzer <- NULL
+	data$Weissenbach <- NULL
+
+# Redundant with Briand
+	data$Newman <- NULL
+
+# Redundant with Brittain
+	data$Larsson <- NULL
+	data$Lien <- NULL
+	data$Lillehammer <- NULL
+	data$Tangen     <- NULL  
+
+# Redundant with Britton
+	data$Kowalczewski <- NULL
+	data$Lack <- NULL
+	data$Matthews <- NULL
+	data$McDonald <- NULL
+
+# Redundant with Brown
+	data$Hechinger <- NULL
+
+# Redundant with Burgis
+	data$Ganf <- NULL
+	data$McGowan <- NULL
+	data$Viner <- NULL
+
+# Redundant with Canuel
+	data$Douglass <- NULL
+	data$Emmett_Duffy <- NULL
+
+# Redundant with Castro
+	data$Filgueira <- NULL
+
+# Redundant with Catella
+	data$Resende <- NULL
+	data$de_Morais <- NULL
+
+# Redundant with Cattaneo.Vietti
+	data$Smith_WO <- NULL
+
+# Redundant with Chen
+  data$Chen_Y <- NULL
+  data$Li_J <- NULL
+  data$Liu <- NULL
+
+# Redundant with Christian
+	data$Luczkovich <- NULL
+
+# Redundant with Clarke_T
+	data$Flechsig <- NULL
+	data$Grigg <- NULL
+
+# Redundant with Clarke_W
+	data$Dayton <- NULL
+	data$Rosenthal <- NULL
+
+# Redundant with Closs
+	data$Lake <- NULL
+
+# Redundant with Coffman
+	data$Cummins <- NULL
+	data$Roff <- NULL
+
+# Redundant with Collins
+	data$Mitchell <- NULL
+	data$Wiegert <- NULL
+
+# Redundant with Conover
+	data$Edwards_D <- NULL
+	data$Sutter <- NULL
+
+# Redundant with Cummins
+	data$Coffman <- NULL
+	data$Roff <- NULL
+
+# Redundant with Dai
+	data$Fang <- NULL
+	data$Hsieh <- NULL
+	data$Hung <- NULL
+	data$Lin <- NULL
+	data$Lo <- NULL
+	data$Shao <- NULL
+	data$Su <- NULL
+
+# Redundant with Darlington
+	data$Moriarty_C <- NULL
+	data$Moriarty_D <- NULL
+	data$Tevlin <- NULL
+
+# Redundant with Detloff
+	data$Josten <- NULL
+	data$Zander <- NULL
+
+# Redundant with Ebenman
+	data$Emmerson <- NULL
+	data$Montoya <- NULL
+	data$Olesen <- NULL
+	data$Valido <- NULL
+
+# Redundant with Edwards_E
+	data$Kilroy <- NULL
+	data$McIntosh <- NULL
+	data$Scarsbrook <- NULL
+
+# Redundant with Feroz_Khan
+	data$Panikkar <- NULL
+
+# Redundant with Fetahi
+	data$Mengistou <- NULL
+	data$Schagerl <- NULL
+
+# Redundant with Fradkin
+	data$Hampton <- NULL
+	data$Leavitt <- NULL
+	data$Rosenberger <- NULL
+
+# Redundant with Garcia.Cantizano 
+	data$Massana <- NULL
+	data$Pedros.Alio <- NULL
+
+# Redundant with Godfrey
+	data$Pearson <- NULL
+	data$Pusey <- NULL
+
+# Redundant with Goldwasser
+	data$Roughgarden <- NULL
+
+# Redundant with Gontikaki
+	data$Soetaert <- NULL
+	data$Witte <- NULL
+	data$van_Oevelen <- NULL
+
+# Redundant with Harris
+	data$Paur <- NULL
+
+# Redundant with Hasham
+	data$Hildrew <- NULL
+
+# Redundant with Heald
+	data$Odum_W <- NULL
+
+# Redundant with Hildrew
+	data$Hasham  <- NULL
+
+# Redundant with Homer
+	data$Kemp  <- NULL
+	data$Lehman  <- NULL
+	data$McKellar  <- NULL
+	data$Odum_H  <- NULL
+	data$Smith_WH  <- NULL
+	data$Young  <- NULL
+
+# Redundant with Hopkins
+	data$Lancraft  <- NULL
+	data$Torres  <- NULL
+
+# Redundant with Huang
+	data$Li_X  <- NULL
+	data$Yan  <- NULL
+
+# Redundant with Hurlbert
+	data$Mulla  <- NULL
+	data$Wilson_H  <- NULL
+
+# Redundant with Johnson
+	data$Orlofske  <- NULL
+	data$Preston  <- NULL
+
+# Redundant with  Lauridsen
+	data$Papantoniou <- NULL
+
+# Redundant with Marques
+	data$Patricio <- NULL
+
+# Redundant with Motta
+	data$Uieda <- NULL
+
+# Redundant with Pattie
+	data$Verbeek <- NULL
+
+# Redundant with Price
+	data$Mayse <- NULL
+
+# Redundant with Ratsirarson 
+	data$Silander <- NULL
+
+# Redundant with Seifert_F
+	data$Seifert_R <- NULL
+
+# Redundant with Shushkina
+	data$Vinogradov <- NULL
+
+# Redundant with Sprules
+	data$Stewart <- NULL
+
+# Redundant with Stagliano
+	data$Whiles  <- NULL  
+
+# Redundant with Tavares.Cromar
+	data$Williams_D <- NULL
+
+
+
+
+	
